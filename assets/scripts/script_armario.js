00f0f9c0e8b6c69c3b4ae1c93d02907a47aba18d
leonardo lopez <leonardohuarcaya27@gmail.com>
2023-06-25 15:35:46 -0500

    agregar al armario

--- assets/scripts/script_armario.js
+++ assets/scripts/script_armario.js
@@ -2,6 +2,7 @@ const defaultFile = 'imagenes/anhadir.png';
 const file = document.getElementById( 'foto');
 const img = document.getElementById ( 'img' );
 file.addEventListener( 'change', e => {
+    console.log(e);
     if( e.target.files[0] ){
         const reader = new FileReader ( );
         reader.onload = function( e ){
@@ -13,3 +14,40 @@ file.addEventListener( 'change', e => {
         img.src = defaultFile;
     }
 });
+
+class Ropa{
+    constructor(img,descripcion){
+        this.img=img;
+        this.descripcion=descripcion;
+    }
+}
+class UI{
+    addRopa(ropa){
+        const lista = document.getElementById('contenedor');
+        const element = document.createElement('div');
+        element.innerHTML=`
+            <div class="galeria">
+                <div class="foto">
+                    <a href=""> <img src="assets/images/${ropa.img.files[0].name}" alt=""></a>
+                </div>
+                <div class="pie">
+                    <p>${ropa.descripcion}</p>
+                </div>
+            </div>
+        `;
+        
+        lista.appendChild(element);
+    }
+    deleteRopa(){
+
+    }
+}
+document.getElementById('formulario').addEventListener('submit',function(e){
+    const foto = document.getElementById ('foto');
+    const descripcion=document.getElementById('descripcion').value;
+    const ropa=new Ropa(foto,descripcion);
+    const ui=new UI();
+    ui.addRopa(ropa);
+    
+    e.preventDefault();
+})
